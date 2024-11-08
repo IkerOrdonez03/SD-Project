a447c41485f8e7cf2829d461932b77df8235f67b
Javier <javier.g@opendeusto.es>
2024-11-08 21:51:43 +0100

    Implementación de métodos en la Façade

--- src/main/java/es/deusto/sd/eurostyletuning/service/EuroStyleTuningService.java
+++ src/main/java/es/deusto/sd/eurostyletuning/service/EuroStyleTuningService.java
@@ -1,7 +1,17 @@
 package es.deusto.sd.eurostyletuning.service;
 
+import java.util.List;
+
+import es.deusto.sd.eurostyletuning.dto.BrandDTO;
+import es.deusto.sd.eurostyletuning.dto.CategoryDTO;
+import es.deusto.sd.eurostyletuning.dto.PartDTO;
 import es.deusto.sd.eurostyletuning.dto.PurchaseRequestDTO;
 
 public interface EuroStyleTuningService {
 	String processPurchase(PurchaseRequestDTO purchaseRequest);
+	List<CategoryDTO> collectCategories();
+	List<BrandDTO> collectCarBrands();
+	List<PartDTO> retrieveParts(int brandId, int categoryId);
+	
+	
 }
