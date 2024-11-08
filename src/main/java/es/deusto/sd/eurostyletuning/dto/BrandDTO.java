d1ed47adb0ec053ac556e62b687fdeb99211ed17
Javier <javier.g@opendeusto.es>
2024-11-08 21:30:38 +0100

    Create BrandDTO

--- src/main/java/es/deusto/sd/eurostyletuning/dto/BrandDTO.java
+++ src/main/java/es/deusto/sd/eurostyletuning/dto/BrandDTO.java
@@ -2,4 +2,32 @@ package es.deusto.sd.eurostyletuning.dto;
 
 public class BrandDTO {
 
+    private int brandID;
+    private String brandName;
+
+ 
+    public BrandDTO(int brandID, String brandName) {
+        this.brandID = brandID;
+        this.brandName = brandName;
+    }
+
+
+    public BrandDTO() {
+    }
+
+    public int getBrandID() {
+        return brandID;
+    }
+
+    public void setBrandID(int brandID) {
+        this.brandID = brandID;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
 }
