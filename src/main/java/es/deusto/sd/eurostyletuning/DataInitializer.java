42b938236e176319dd5ad897acc003608e109e58
Javier <javier.g@opendeusto.es>
2024-11-13 18:10:58 +0100

    legibilidad en DataInitializater

--- src/main/java/es/deusto/sd/eurostyletuning/DataInitializer.java
+++ src/main/java/es/deusto/sd/eurostyletuning/DataInitializer.java
@@ -50,36 +50,37 @@ public class DataInitializer {
 		logger.info("Categoria {" + p + "} guardada con éxito.");
 	}
 
-     */
+     
 
 //    @Bean
-//    CommandLineRunner initData(EuroStyleTuningService tuningService) {
-//        return args -> {
-//            // Crear algunas marcas de automóviles
-//            CarBrand audi = new CarBrand(1L, "Audi");
-//            CarBrand ford = new CarBrand(2L, "Ford");
-//            CarBrand nissan = new CarBrand(3L, "Nissan");
-//
-//            // Simulación de almacenamiento de marcas en el servicio
-//            List<CarBrand> brands = List.of(audi, ford, nissan);
-//            brands.forEach(brand -> {
-//                tuningService.addBrand(brand); // Almacena en el servicio
-//                logger.info("Marca guardada: " + brand.getName());
-//            });
-//
-//            // Crear piezas para cada marca
-//            Part steeringWheel = new Part(1L, "Steering Wheel", 150.00, 10, audi.getId());
-//            Part seatCushion = new Part(2L, "Seat Cushion", 75.00, 5, ford.getId());
-//            Part carMat = new Part(3L, "Car Mat", 50.00, 15, nissan.getId());
-//
-//            // Simulación de almacenamiento de piezas en el servicio
-//            List<Part> parts = List.of(steeringWheel, seatCushion, carMat);
-//            parts.forEach(part -> {
-//                tuningService.addPart(part); // Almacena en el servicio
-//                logger.info("Pieza guardada: " + part.getName() + ", precio: " + part.getPrice());
-//            });
-//
-//            logger.info("Datos iniciales cargados para EuroStyleTuning!");
-//        };
-//    }
+    CommandLineRunner initData(EuroStyleTuningService tuningService) {
+        return args -> {
+            // Crear algunas marcas de automóviles
+            CarBrand audi = new CarBrand(1L, "Audi");
+            CarBrand ford = new CarBrand(2L, "Ford");
+            CarBrand nissan = new CarBrand(3L, "Nissan");
+
+            // Simulación de almacenamiento de marcas en el servicio
+            List<CarBrand> brands = List.of(audi, ford, nissan);
+            brands.forEach(brand -> {
+                tuningService.addBrand(brand); // Almacena en el servicio
+                logger.info("Marca guardada: " + brand.getName());
+            });
+
+            // Crear piezas para cada marca
+            Part steeringWheel = new Part(1L, "Steering Wheel", 150.00, 10, audi.getId());
+            Part seatCushion = new Part(2L, "Seat Cushion", 75.00, 5, ford.getId());
+            Part carMat = new Part(3L, "Car Mat", 50.00, 15, nissan.getId());
+
+            // Simulación de almacenamiento de piezas en el servicio
+            List<Part> parts = List.of(steeringWheel, seatCushion, carMat);
+            parts.forEach(part -> {
+                tuningService.addPart(part); // Almacena en el servicio
+                logger.info("Pieza guardada: " + part.getName() + ", precio: " + part.getPrice());
+            });
+
+            logger.info("Datos iniciales cargados para EuroStyleTuning!");
+        };
+    }
+    */
 }
